3e3e972eb763fdcfac0e548fa61b58d930e77c56
Alina Sorger <alina.sorger@ebcont.com>
2023-05-23 18:13:40 +0200

    feat: created route for cart

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
 import {ShopContainerComponent} from "./views/shop-container/shop-container.component";
 import {ShopComponent} from "./components/shop/shop.component";
+import {CartContainerComponent} from "./views/cart-container/cart-container.component";
 
 export const APP_ROUTES: Routes = [
   { path: '', pathMatch: 'full', redirectTo: ''},
   // { path: 'shop', loadComponent: () => import('./views/shop-container/shop-container.component').then((c => c.ShopContainerComponent))},
   { path: 'shop', component: ShopContainerComponent},
-  { path: 'test', component: ShopComponent}
+  { path: 'cart', component: CartContainerComponent}
 ]
