5a262f207ffd0864a4808f1ee8bb6e78cba56e48
Alina Sorger <alina.sorger@ebcont.com>
2023-05-25 15:03:51 +0200

    feat: created shopservice and product interface

---
+++ src/app/product.ts
@@ -0,0 +1,6 @@
+export interface Product {
+  id: string | '1';
+  image_url: string | 'url';
+  price: string | '5';
+  name: string | 'name';
+}
---
+++ src/app/shop.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { Product} from "./product";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ShopService {
+
+  // constructor(private Product: Product) {}
+
+
+
+}
