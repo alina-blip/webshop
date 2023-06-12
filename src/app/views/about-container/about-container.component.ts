28dd353727c0f3a1a34c9028fb4ceabbd9f1b864
Alina Sorger <alina.sorger@ebcont.com>
2023-06-12 17:22:49 +0200

    feat: create about component

---
+++ src/app/views/about-container/about-container.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AboutComponent } from '../../components/about/about.component'
+
+@Component({
+  selector: 'app-about-container',
+  standalone: true,
+  imports: [CommonModule, AboutComponent],
+  templateUrl: './about-container.component.html',
+  styleUrls: ['./about-container.component.scss']
+})
+export class AboutContainerComponent {
+
+}
