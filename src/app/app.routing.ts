65fc560198c6237a8dfce8daa5c56edcdb1c6bbb
Alina Sorger <alina.sorger@ebcont.com>
2023-06-12 17:22:23 +0200

    feat: update routing for new components

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,10 +3,14 @@ import {ProductsContainerComponent} from "./views/products-container/products-co
 import {ProductsComponent} from "./components/products/products.component";
 import {CartContainerComponent} from "./views/cart-container/cart-container.component";
 import {ProductDetailContainerComponent} from "./views/product-detail-container/product-detail-container.component";
+import { AboutContainerComponent } from './views/about-container/about-container.component'
+import { WorksContainerComponent } from './views/works-container/works-container.component'
 
 export const APP_ROUTES: Routes = [
   { path: '', pathMatch: 'full', redirectTo: ''},
   { path: 'products', loadComponent: () => import('./views/products-container/products-container.component').then((c => c.ProductsContainerComponent))},
   { path: 'cart', component: CartContainerComponent},
-  { path: 'product', component: ProductDetailContainerComponent}
+  { path: 'product', component: ProductDetailContainerComponent},
+  { path: 'about', component: AboutContainerComponent},
+  { path: 'works', component: WorksContainerComponent},
 ]
