7dc30b5e67518d68a1d080b0ce56455346cd7c1f
Alina Sorger <alina.sorger@ebcont.com>
2023-06-27 16:06:55 +0200

    feat: update routes

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,8 +3,10 @@ import { CartContainerComponent } from './views/cart-container/cart-container.co
 import { ProductDetailContainerComponent } from './views/product-detail-container/product-detail-container.component';
 import { AboutContainerComponent } from './views/about-container/about-container.component';
 import { WorksContainerComponent } from './views/works-container/works-container.component';
-import { AdminContainerComponent } from './views/admin-container/admin-container.component';
+import { LoginContainerComponent } from './views/login-container/login-container.component';
 import { StorageContainerComponent } from './views/storage-container/storage-container.component';
+import { ContactContainerComponent } from './views/contact-container/contact-container.component'
+import { FaqContainerComponent } from './views/faq-container/faq-container.component'
 
 export const APP_ROUTES: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '' },
@@ -19,6 +21,9 @@ export const APP_ROUTES: Routes = [
   { path: 'product/:id', component: ProductDetailContainerComponent },
   { path: 'about', component: AboutContainerComponent },
   { path: 'works', component: WorksContainerComponent },
-  { path: 'admin', component: AdminContainerComponent },
+  { path: 'login', component: LoginContainerComponent },
   { path: 'upload', component: StorageContainerComponent },
+  { path: 'contact', component: ContactContainerComponent },
+  { path: 'faqs', component: FaqContainerComponent }
+
 ];
